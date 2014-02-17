f41691b0bd334ea1e89ea5716d63e644e3cd9e04
Makis Otman <makisotman@gmail.com>
2014-02-17 11:45:58 +0000

    added html structure and css. Header is now fixed to the top

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -8,6 +8,6 @@ class Peep
   property :created_at, Time
 
   def time_format
-    created_at.strftime("On %A (%d/%m/%y) , at %I:%M%p")
+    created_at.strftime("On %A (%d/%m/%y), at %I:%M%p")
   end
 end
